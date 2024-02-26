1a7ad2eda7bc1e92ea3ef4da99102fc2a8119f10
xxkolibryxx <artyomen2@gmail.com>
2024-02-26 15:35:30 +0400

    create filterArray function

--- Program.cs
+++ Program.cs
@@ -1,1 +1,14 @@
-﻿
+﻿string[] initArray = ["Hello", "2", "world", ":-)"];
+
+static string[] filterArray(string[] array)
+{
+    List<string> output = [];
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3)
+        {
+            output.Add(array[i]);
+        }
+    }
+    return [.. output];
+}
