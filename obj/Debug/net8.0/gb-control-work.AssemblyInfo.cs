84c2c496fbe6ad28a9bb3148509e56dc75d0e696
xxkolibryxx <artyomen2@gmail.com>
2024-02-26 16:47:54 +0400

    Add Block Schema Image

--- obj/Debug/net8.0/gb-control-work.AssemblyInfo.cs
+++ obj/Debug/net8.0/gb-control-work.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("gb-control-work")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1a1078f28025e7ae1d861e4eb7851c69abc08bb0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e16ab82a9ebb1eb06340c5cabeb0cba510818e1f")]
 [assembly: System.Reflection.AssemblyProductAttribute("gb-control-work")]
 [assembly: System.Reflection.AssemblyTitleAttribute("gb-control-work")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
